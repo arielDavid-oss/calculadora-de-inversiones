cb654c3cc845ed34ee9086d5e48af5ed70a87603
David Hernandez <arielhernandez03@outlook.com>
2024-08-29 08:20:35 -0600

    primer comit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
